0ca05939f919ba9a2a961c5599f603e99b0f8188
danielpqeuni <daniel.quispe.e@uni.pe>
2018-03-20 22:51:52 -0500

    adding jade

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app=express();
 // express: sirve para crear un micro entorno para servidores
-
+app.set('view engine','pug')
 app.get('/',function (req,res) {
-    res.send('Hola mundo!')
+    res.render('index')
 });
 
 app.listen(3000,function (err) {
