0a6266d09b4577d248f441f83da455eb9405a3cc
danielpqeuni <daniel.quispe.e@uni.pe>
2018-03-24 10:49:07 -0500

    routes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var page=require('page');
+var main=document.getElementById('main-container');
+
+page('/',function (ctx,next) {
+    main.innerHTML='HOME <a href="/signup">Signup</a>';
+})
+
+page('/signup',function (ctx,next) {
+    main.innerHTML='SIGNUP <a href="/">Home</a>'
+})
+page.start()
